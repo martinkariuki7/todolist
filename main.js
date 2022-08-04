3edac59f128d787c1d9aba6acf0ef53f124e4498
Martin Kariuki <martinkariuki7@gmail.com>
2022-08-04 11:02:16 +0300

    Added ability to mark tasks as done

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const newToDo = document.getElementById('todo-new')
 const addBtn = document.getElementById('addToDo')
 const main= document.getElementById('toDoList')
-const taskDone = document.getElementsByClassName('task')
 const alertArea = document.getElementById('alert')
 
 let toDoList = []
@@ -15,14 +14,17 @@ function populateToDo(){
   let todo = newToDo.value.trim()
   oldToDoList = [...toDoList]
 
+  // Store todos in object
+  const toDoObject = {
+    name: todo,
+    status: true
+  }
+
   if(todo === null || todo === '' ){
     alertArea.innerHTML = `<p class="alert">You must write something</p>`
   } else {
-    toDoList.push(todo) 
-    // Add each task to localstorage
+    toDoList.push(toDoObject) 
     localStorage.setItem('ToDoList', JSON.stringify(toDoList))
-
-    populateUI()
     updateDOM()
   }
 
@@ -39,16 +41,37 @@ function populateUI(){
 
 //Update DOM
 function updateDOM(){
-    // Check if items already exist
-    toDoList.forEach(item => {
-        if(!oldToDoList.includes(item)){
+
+    // Show the latest task
+    toDoList.filter(item => {
+        if(!oldToDoList.includes(item)){ 
+          
+            // Show task on the front
             let task = document.createElement('div')
             task.classList.add('task')
-            task.innerHTML = `<label><input type="checkbox" >${item}</label><br>`
-            main.append(task)
-    
-            task.addEventListener('change', () => task.classList.toggle('task-complete'))
+            task.innerHTML = `<label><input type="checkbox" >${item.name}</label><br>`
+            
+            // Mark completed items
+            if(!item.status){
+                task.querySelector('input').checked = true
+                task.classList.toggle('task-complete')
             }
+
+            main.append(task)
+            
+            // Toggle items as done/ not done
+            task.addEventListener('change', () => {
+                // Update toDoObject
+                item.status ? item.status = false
+                : !item.status ? item.status = true 
+                : item.status              
+
+                task.classList.toggle('task-complete')
+                localStorage.setItem('ToDoList', JSON.stringify(toDoList))
+                
+            })
+
+          }
     })
 
     newToDo.value = ''  
